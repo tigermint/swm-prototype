886ba7326c715c8d5bf65723d127e270854b484d
LEESEUNGYEOL <lsy12210@gmail.com>
2023-05-10 21:23:38 +0900

    feat: Footer Color change

--- src/components/atoms/NavButton/index.jsx
+++ src/components/atoms/NavButton/index.jsx
@@ -5,17 +5,21 @@ const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+    :hover {
+        cursor: pointer;
+    }
 `
 const IconSection = styled.div`
     margin-bottom: 0.5rem;
 `
 const LabelSection = styled.div`
-    font-size: 1rem;
+    font-size: 0.9rem;
     font-weight: bold;
     color: ${props => props.color};
 `
 
 const NavButton = ({ icon, text, color, onClick }) => {
+
     return (
         <Wrapper onClick={onClick}>
             <IconSection>{icon}</IconSection>
--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -1,10 +1,14 @@
 import { styled } from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHouseChimney, faRightFromBracket, faBook } from '@fortawesome/free-solid-svg-icons'
+import { faBuilding, faCalendar, faShareFromSquare } from '@fortawesome/free-regular-svg-icons'
+import { useState, useEffect } from "react";
+import { memo } from "react";
 
 //component
 import NavButton from '../atoms/NavButton';
+
+
 
 const Wrapper = styled.div`
     position: sticky;
@@ -27,17 +31,24 @@ const NavListSection = styled.div`
 
 const Footer = () => {
     const navigate = useNavigate();
+    const [space, setSpace] = useState("");
+    const [reservation, setReservation] = useState("");
+
+    useEffect(() => {
+        window.location.pathname === "/space" ? setSpace("#3864FF") : setSpace("#656A7B");
+        window.location.pathname === "/me" ? setReservation("#3864FF") : setReservation("#656A7B");
+    }, [])
 
     return (
         <Wrapper>
             <NavListSection>
-                <NavButton icon={<FontAwesomeIcon icon={faHouseChimney} size="xl" />} text={"회의실"} onClick={() => {
+                <NavButton icon={<FontAwesomeIcon icon={faBuilding} size="xl" color={space} />} text={"회의실"} color={space} onClick={() => {
                     navigate("/space");
                 }} />
-                <NavButton icon={<FontAwesomeIcon icon={faBook} size="xl" />} text={"예약내역"} onClick={() => {
+                <NavButton icon={<FontAwesomeIcon icon={faCalendar} size="xl" color={reservation} />} text={"예약내역"} color={reservation} onClick={() => {
                     navigate("/me");
                 }} />
-                <NavButton icon={<FontAwesomeIcon icon={faRightFromBracket} size="xl" />} text={"로그아웃"} onClick={() => {
+                <NavButton icon={<FontAwesomeIcon icon={faShareFromSquare} size="xl" color={"#656A7B"} />} text={"로그아웃"} color={"#656A7B"} onClick={() => {
                     navigate("/");
                 }} />
             </NavListSection>
