eb030f21cc636198b6764745ce3f1cd374eb9341
Basak <chgy2131@naver.com>
2023-05-10 16:21:55 +0900

    refact: supabase component 분리 및 환경변수 추가

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,24 +1,7 @@
 import {Input} from '@mantine/core';
-import {useState, useEffect} from 'react';
-import {createClient} from '@supabase/supabase-js';
+import { supabase } from "../utils/supabaseClient";
 
 const TestPage = () => {
-    // set supabase component
-    const [supabase, setSupabase] = useState(null);
-
-    const supabaseUrl = 'https://nzhaegdvcmeuxnbfawjc.supabase.co';
-    const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im56aGFlZ2R2Y21ldXhuYmZhd2pjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODM1MDk4MTQsImV4cCI6MTk5OTA4NTgxNH0.jmiGu0fzMv-rF07o8noAwg_Posu-ia_MJrxCMzZ_Ub4';
-
-    useEffect(() => {
-        const supabaseClient = createClient(supabaseUrl, supabaseKey);
-        setSupabase(supabaseClient);
-    }, []);
-
-    if (!supabase) {
-        return <div>Loading...</div>;
-    }
-
-    // set supabase oauth logic
     const handleLogin = async (googleUser) => {
         // const { id_token } = googleUser.clientId;
         const {data, error} = await supabase.auth.signInWithOAuth({
--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,8 +1,30 @@
+import {useEffect, useState} from "react";
+import {supabase} from "../utils/supabaseClient";
+
 const WelcomePage = () => {
-  return (
-    <div>
-      <h1>MywwwPage</h1>
-    </div>
-  );
+    const dbGetUserByMyUuid = async() => {
+        const { data, error } = await supabase
+            .from('user')
+            .select()
+            //.eq('uuid', user.email)
+
+        // 결과 확인
+        if (error) {
+            console.error(error);
+        } else {
+            console.log(data);
+            console.log(supabase);
+        }
+    }
+    dbGetUserByMyUuid();
+
+    // case1(#1): registered user
+    // case2(#1-1): new user
+    // next case(#1-2): all
+    return (
+        <div>
+            <h1>MywwwPage</h1>
+        </div>
+    );
 };
 export default WelcomePage;
