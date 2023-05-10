fa7f60eeaef850f509b13737fb908e820b93aa3e
Basak <chgy2131@naver.com>
2023-05-11 00:38:34 +0900

    refact: api 구문 함수단위로 분리

--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,35 +1,38 @@
 import {useEffect, useState} from "react";
 import {supabase} from "../utils/supabaseClient";
 
-const WelcomePage = () => {
-    const [userInform, setUserInform] = useState(null);
-
-    // get uuid from supabase.auth
-    useEffect(() => {
-        const dbGetAuth = async () => {
-            const {data: {user}, error} = await supabase.auth.getUser()
-
-            // 결과 확인
-            if (error) {
-                console.error(error);
-            } else {
-                setUserInform(user?.id);
-            }
-        }
-        dbGetAuth();
-    }, []);
-
-    // create new user
-    const dbPutUser = async (name) => {
+/**
+ * user 테이블에 uuid를 기준으로 name을 신규 등록한다.
+ * @param uuid
+ * @param name
+ * @returns {(function(*): Promise<void>)|*}
+ */
+function dbPutUser(uuid, name) {
+    return async () => {
+        console.log("1111111111112");
         const {data, error} = await supabase
             .from('user')
             .insert([
-                {uuid: userInform, name: name},
+                {uuid: uuid, name: name},
             ])
-    }
+        console.log("111111111111");
 
-    // get or create user
-    const dbGetUserOrCreate = async () => {
+        // 결과 확인
+        if (error) {
+            console.error(error);
+        } else {
+            console.log(data);
+        }
+    };
+}
+
+/**
+ * User 데이터베이스에 동일한 uuid가 존재하는지 확인
+ * @param userInform
+ * @returns {(function(): Promise<boolean|undefined>)|*} , 유저존재:true, 유저없음:false
+ */
+function dbCheckUser(userInform) {
+    return async () => {
         //await dbGetAuth();
 
         const {data, error} = await supabase
@@ -41,18 +44,50 @@ const WelcomePage = () => {
         if (error) {
             console.error(error);
             if (error.code === 'PGRST116') {  // 검색된 기록이 없을때
-                console.log("가입되지않은 사용자입니다.");
-                await dbPutUser("배고파");
+                return false;
             }
         } else {
             console.log(data[0]);
             if (data[0] === undefined) {
-                console.log("가입되지않은 사용자입니다.");
-                await dbPutUser("홈길동");
+                return false;
             }
+            return true;
         }
-    }
-    dbGetUserOrCreate();
+    };
+}
+
+function dbGetLoginedAuth(setUserInform) {
+    return async () => {
+        const {data: {user}, error} = await supabase.auth.getUser()
+
+        // 결과 확인
+        if (error) {
+            console.error(error);
+        } else {
+            setUserInform(user?.id);
+        }
+    };
+}
+
+const WelcomePage = () => {
+    const [userInform, setUserInform] = useState(null);
+
+    // get uuid from supabase.auth
+    useEffect(() => {
+        const dbGetAuth = dbGetLoginedAuth(setUserInform);
+        dbGetAuth();
+    }, []);
+
+    // create new user
+    const dbGetUserOrCreate = dbCheckUser(userInform);
+    const dbCreateUser = dbPutUser(userInform, "홈홈홈");
+    dbGetUserOrCreate().then(async (value) => {
+        console.log(value);
+        if (value === false) {
+            console.log("가입되지않은 사용자입니다.");
+            await dbCreateUser();
+        }
+    });
 
     // rend
     return (
