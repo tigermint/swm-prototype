980ff5d7fe6e480f74c54c7735876ec0b1a1d831
Basak <chgy2131@naver.com>
2023-05-13 02:44:35 +0900

    feat: DB에서 소속 조직에 Space들을 가져오는 기능 추가

--- src/pages/creator/SpacePage.jsx
+++ src/pages/creator/SpacePage.jsx
@@ -1,11 +1,14 @@
 import {useLocation, useNavigate} from "react-router-dom";
 import MainLayout from "../../components/MainLayout"
 import SpaceGrid from "../../components/organisms/SpaceGrid";
+//apis
+import {protoFindAllSpaceByOrganizationId} from "../../apis/supabaseProto";
 
 const SpacePage = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
+  // organization 정보 가져오기
   const {organization_id = 0} = location.state || {};
   if (organization_id === 0) {
     console.error("올바르지 않은 Space 페이지 접근입니다. 조직 찾기 페이지에서 시작해주세요.");
@@ -14,6 +17,14 @@ const SpacePage = () => {
   } else {
     console.log(organization_id);
   }
+
+  // custom method
+  const listUpSpace = (organization_id) => {
+    protoFindAllSpaceByOrganizationId(organization_id).then((spaces) => {
+      console.log(spaces);
+    })
+  }
+  listUpSpace(organization_id);
 
   const handleSpace = (index) => {
     console.log(index);
